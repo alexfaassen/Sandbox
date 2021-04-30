42cb01e4dc59935ae798d5b4ca7605a4f3a759a4
Alex Faassen <alexdrfaassen@gmail.com>
2021-04-30 12:23:02 -0400

    This is a test

---
+++ test_c.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    cout << "Hello World" << endl;
+/*
+    //variables
+    int x = 0;
+
+    //command line inputs
+    for(int i = 0; i < 5; i++){
+        x += i;
+    }
+
+    cout << x << endl;
+
+    //set random seed if seed hasn't been set
+    if(!setSeed){
+        srand(time(NULL));
+    } else {
+        srand(seed);
+    }
+
+    return 1;
+*/
+}
+
